0be2ac6ca5a4abb7d70e6f390ef03f482cc7bd81
Andrey <rolland81280@gmail.com>
2022-09-08 23:28:27 +0300

    сохран6

--- exercise-15/Program.cs
+++ exercise-15/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру, обозначающую день недели");
+string name = Console.ReadLine();
+int number = Convert.ToInt32(name);
+if(number < 1)
+    {
+        Console.WriteLine("Цифра не соответствует дню недели");
+    }
+else
+{
+    if(number > 7)
+    {
+        Console.WriteLine("Цифра не соответствует дню недели");
+    }
+    else
+    {
+        if(number < 6)
+        {
+            Console.WriteLine("Это будний день");
+        }
+        else
+        {
+            if(number > 5)
+            {
+                Console.WriteLine("Это выходной день");
+            }
+        }
+    }
+}
