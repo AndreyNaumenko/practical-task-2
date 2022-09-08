c7b047bb3d3465b84fada7d34be71fd4df25b973
Andrey <rolland81280@gmail.com>
2022-09-08 21:14:54 +0300

    сохран3

--- exercise-10/Program.cs
+++ exercise-10/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число");
+string name = Console.ReadLine();
+int number = Convert.ToInt32(name);
+int numberTwo = 0;
+
+numberTwo = number / 10;
+numberTwo = numberTwo % 10;
+
+Console.WriteLine("Вторая цифра этого числа " + numberTwo);
