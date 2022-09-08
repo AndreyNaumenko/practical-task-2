7402bd28105c49039ea3911a0b65c499f6c84d87
Andrey <rolland81280@gmail.com>
2022-09-08 22:18:58 +0300

    сохран2

--- exercise-13/Program.cs
+++ exercise-13/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число");
+string name = Console.ReadLine();
+int number = Convert.ToInt32(name);
+int numbertThree = 0;
+
+if(number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+    number = number / 10;
+    }
+       numbertThree = number % 10;
+       Console.WriteLine("Третья цифра заданного числа " + numbertThree);
+}
